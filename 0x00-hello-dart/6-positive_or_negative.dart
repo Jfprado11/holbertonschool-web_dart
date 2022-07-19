5b676cc09d8b3c6c0ff0c3edfd9e6afb139f8318
Felipe Prado <jfpc11@misena.edu.co>
2022-07-18 22:40:02 -0500

    adding semicolon

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  int numArg = int.parse(args[0])
+  int numArg = int.parse(args[0]);
   if (numArg > 0) {
     print('${numArg} is positive');
   } else if (numArg == 0) {
