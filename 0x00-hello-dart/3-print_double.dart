9966d7b4dbed9970b93ef0f4420af72231d30beb
Felipe Prado <jfpc11@misena.edu.co>
2022-07-18 22:13:09 -0500

    trying with other

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,6 +1,6 @@
 void main() {
   var n = 3.14159265359;
-  double newNum = double.parse(n.toStringAsPrecision(3));
+  double newNum = double.parse(n.toStringAsFixed(2));
   print('Double: $newNum\n');
 }
 
